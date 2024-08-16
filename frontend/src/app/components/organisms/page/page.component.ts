1f9ea69a7f90601eb851152bf8dd9366bc9c6752
Elif Gokce <e_kaya0@outlook.com>
2024-08-16 17:37:48 +0300

    bug fixed

--- frontend/src/app/components/organisms/page/page.component.ts
+++ frontend/src/app/components/organisms/page/page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchComponent } from '../../atoms/search/search.component';
 import { IconComponent } from '../../atoms/icon/icon.component';
 import { SearchButtonGroupComponent } from '../search-button-group/search-button-group.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [SearchComponent, IconComponent, SearchButtonGroupComponent],
+  imports: [SearchComponent, IconComponent, SearchButtonGroupComponent, TableComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
