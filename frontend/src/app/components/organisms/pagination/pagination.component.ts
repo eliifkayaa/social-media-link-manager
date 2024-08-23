1a3aaa270f6d33d59d768ff324986285a73f8cda
Elif Gokce <e_kaya0@outlook.com>
2024-08-23 10:28:51 +0300

    pagination design completed

--- frontend/src/app/components/organisms/pagination/pagination.component.ts
+++ frontend/src/app/components/organisms/pagination/pagination.component.ts
@@ -1,18 +1,41 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { SharedModule } from '../../../commons/modules/shared/shared.module';
+import { TextComponent } from '../../atoms/text/text.component';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [MatPaginatorModule],
+  imports: [SharedModule, TextComponent],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
-export class PaginationComponent implements AfterViewInit{
+export class PaginationComponent {
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  ngAfterViewInit(): void {
-    // this.dataSource.paginator = this.paginator;
+  currentPage = 1;
+  totalPages = 4;  // Total number of pages
+  rowsPerPage = 4;
+  rowsOptions = [4, 8, 12, 16];
+
+  onRowsChange() {
+    this.currentPage = 1; // Reset to first page when rows per page changes
+    this.calculateTotalPages();
   }
 
+  calculateTotalPages() {
+    // Assuming you have a method to calculate the total number of pages based on rows per page.
+    // This can be done based on total number of items.
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
+
